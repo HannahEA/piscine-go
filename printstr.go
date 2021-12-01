0878dde39cfde3aba025000bf14de5e22a1792c6
Hannah.Alemu <hnnh.almu@gmail.com>
2021-12-01 17:16:49 +0000

    prinstring

--- printstr.go
+++ printstr.go
@@ -1,7 +1,12 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func PrintStr(s string) {
 	schangeable := []rune(s)
-	sfinalised := string(schangeable)
-	println(sfinalised)
+
+	for i := 0; i <= 11; i++ {
+		z01.PrintRune(schangeable[i])
+	}
+	z01.PrintRune('\n')
 }
